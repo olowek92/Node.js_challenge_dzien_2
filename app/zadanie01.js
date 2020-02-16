0e72675299b5762fa53c57a1c738a194637291f9
Wojciech <olowek92@gmail.com>
2020-02-16 01:32:09 +0100

    First task done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,19 @@
-//Twój kod
+const fs = require("fs");
+
+fs.readFile("./data/zadanie01/input.json", "utf8", (err, data) => {
+  if (err === null) {
+    let sum = 0;
+    JSON.parse(data).forEach(e => {
+      sum += e;
+    });
+    fs.writeFile("./data/zadanie01/sum.txt", sum, err => {
+      if (err === null) {
+        console.log("Hurra!");
+      } else {
+        console.log("Error: " + err);
+      }
+    });
+  } else {
+    console.log("Something went wrong :(");
+  }
+});
