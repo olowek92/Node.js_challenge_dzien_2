3b678a676a650a74a59ee87321396c9824e2d1d8
Wojciech <olowek92@gmail.com>
2020-02-16 21:15:36 +0100

    Task of a day done

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,19 @@
-//Twój kod
+const fs = require("fs");
+
+process.argv.forEach((val, index) => {
+  if (index > 1) {
+    fs.readFile(val, "utf8", (err, data) => {
+      if (err === null) {
+        let changingLetters = data
+          .split("")
+          .map((letter, index) =>
+            index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()
+          )
+          .join("");
+        console.log(changingLetters);
+      } else {
+        console.log("Error: ", err);
+      }
+    });
+  }
+});
