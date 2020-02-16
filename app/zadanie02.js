8bc47008f96829ba9d008a832691aa40a9034922
Wojciech <olowek92@gmail.com>
2020-02-16 19:56:38 +0100

    Second task done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,17 @@
-//Twój kod
+const fs = require("fs");
+
+fs.readdir("./data/zadanie02", (err, data) => {
+  if (err === null) {
+    data.forEach(el => {
+      fs.readFile("./data/zadanie02/" + el, "utf8", (err, data) => {
+        if (err === null) {
+          console.log(data);
+        } else {
+          console.log("readFile Error!");
+        }
+      });
+    });
+  } else {
+    console.log("readdir Error!", err);
+  }
+});
